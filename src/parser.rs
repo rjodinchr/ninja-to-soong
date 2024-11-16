1a7f45d6bee29f26fc4512b95a7ec5de2ba2ce88
Romaric Jodin <rjodin@chromium.org>
2024-11-16 12:37:40 +0100

    clean parser

--- src/parser.rs
+++ src/parser.rs
@@ -5,80 +5,114 @@ use std::io::Read;
 use crate::target::BuildTarget;
 use crate::utils::error;
 
+fn parse_output_section(section: &str) -> Result<(Vec<String>, Vec<String>), String> {
+    let mut split = section.split("|");
+    if split.clone().count() < 1 {
+        return error!(format!("parse_output_section failed: '{section}'"));
+    }
+    let mut target_outputs: Vec<String> = Vec::new();
+    for output in split.nth(0).unwrap().trim().split(" ") {
+        target_outputs.push(String::from(output.trim()));
+    }
+    let mut target_implicit_outputs: Vec<String> = Vec::new();
+    if let Some(implicit_outputs) = split.next() {
+        for implicit_output in implicit_outputs.trim().split(" ") {
+            target_implicit_outputs.push(String::from(implicit_output.trim()));
+        }
+    }
+    return Ok((target_outputs, target_implicit_outputs));
+}
+
+fn parse_input_and_rule_section(section: &str) -> Result<(String, Vec<String>), String> {
+    let mut split = section.trim().split(" ");
+    if split.clone().count() < 1 {
+        return error!(format!("parse_input_and_rule_section failed: '{section}'"));
+    }
+    let target_rule = String::from(split.nth(0).unwrap());
+    let mut target_inputs: Vec<String> = Vec::new();
+    for input in split {
+        target_inputs.push(String::from(input.trim()));
+    }
+    return Ok((target_rule, target_inputs));
+}
+
+fn parse_input_and_dep_section(
+    section: &str,
+) -> Result<(String, Vec<String>, Vec<String>), String> {
+    let mut split = section.split("|");
+    let split_count = split.clone().count();
+    if split_count != 1 && split_count != 2 {
+        return error!(format!("parse_input_and_dep_section failed: '{section}'"));
+    }
+
+    let (target_rule, target_inputs) = match parse_input_and_rule_section(split.nth(0).unwrap()) {
+        Ok(return_values) => return_values,
+        Err(err) => return Err(err),
+    };
+
+    let mut target_implicit_dependencies: Vec<String> = Vec::new();
+    if let Some(implicit_dependencies) = split.next() {
+        for implicit_dep in implicit_dependencies.trim().split(" ") {
+            target_implicit_dependencies.push(String::from(implicit_dep.trim()));
+        }
+    }
+    return Ok((target_rule, target_inputs, target_implicit_dependencies));
+}
+
+fn parse_input_section(
+    section: &str,
+) -> Result<(String, Vec<String>, Vec<String>, Vec<String>), String> {
+    let mut split = section.split("||");
+    let split_count = split.clone().count();
+    if split_count != 1 && split_count != 2 {
+        return error!(format!("parse_input_section failed: '{section}'"));
+    }
+
+    let (target_rule, target_inputs, target_implicit_dependencies) =
+        match parse_input_and_dep_section(split.nth(0).unwrap()) {
+            Ok(return_values) => return_values,
+            Err(err) => return Err(err),
+        };
+
+    let mut target_order_only_dependencies: Vec<String> = Vec::new();
+    if let Some(order_only_dependencies) = split.next() {
+        for dep in order_only_dependencies.trim().split(" ") {
+            target_order_only_dependencies.push(String::from(dep.trim()));
+        }
+    }
+    return Ok((
+        target_rule,
+        target_inputs,
+        target_implicit_dependencies,
+        target_order_only_dependencies,
+    ));
+}
+
 fn parse_build_target(line: &str, lines: &mut std::str::Lines<'_>) -> Result<BuildTarget, String> {
     let Some(line_stripped) = line.strip_prefix("build") else {
         return error!(format!("No build prefix: '{line}'"));
     };
 
-    let mut split_column = line_stripped.split(":");
-    let split_column_count = split_column.clone().count();
-    if split_column_count != 2 {
+    let mut split = line_stripped.split(":");
+    let split_count = split.clone().count();
+    if split_count != 2 {
         return error!(format!(
-            "Error while parsing column (expected 2, got {0}): '{split_column:#?}'",
-            split_column_count,
+            "Error while parsing column (expected 2 splits, got {0}): '{split:#?}'",
+            split_count,
         ));
-    }
-    let output_section = split_column.nth(0).unwrap();
-    let input_section = split_column.nth(0).unwrap();
-
-    let mut split_pipe_output_section = output_section.split("|");
-    if split_pipe_output_section.clone().count() < 1 {
-        return error!(format!(
-            "Error while parsing output section for pipe: '{output_section}'"
-        ));
-    }
-    let mut target_outputs: Vec<String> = Vec::new();
-    for output in split_pipe_output_section.nth(0).unwrap().trim().split(" ") {
-        target_outputs.push(String::from(output.trim()));
-    }
-    let mut target_implicit_outputs: Vec<String> = Vec::new();
-    if let Some(implicit_outputs) = split_pipe_output_section.next() {
-        for implicit_output in implicit_outputs.trim().split(" ") {
-            target_implicit_outputs.push(String::from(implicit_output.trim()));
-        }
     }
 
-    let mut split_pipe_input_section = input_section.split("||");
-    let split_pipe_input_section_count = split_pipe_input_section.clone().count();
-    if split_pipe_input_section_count != 1 && split_pipe_input_section_count != 2 {
-        return error!(format!(
-            "Error while parsing input section for double pipe: '{input_section}'"
-        ));
-    }
-    let input_and_dep_section = split_pipe_input_section.nth(0).unwrap();
+    let (target_outputs, target_implicit_outputs) =
+        match parse_output_section(split.nth(0).unwrap()) {
+            Ok(return_values) => return_values,
+            Err(err) => return Err(err),
+        };
+    let (target_rule, target_inputs, target_implicit_dependencies, target_order_only_dependencies) =
+        match parse_input_section(split.nth(0).unwrap()) {
+            Ok(return_values) => return_values,
+            Err(err) => return Err(err),
+        };
 
-    let mut split_pipe_input_and_dep_section = input_and_dep_section.split("|");
-    let split_pipe_input_and_dep_section_count = split_pipe_input_and_dep_section.clone().count();
-    if split_pipe_input_and_dep_section_count != 1 && split_pipe_input_and_dep_section_count != 2 {
-        return error!(format!(
-            "Error while parsing input section for pipe: '{input_and_dep_section}'"
-        ));
-    }
-    let inputs_and_rule = split_pipe_input_and_dep_section.nth(0).unwrap();
-
-    let mut split_inputs_and_rule = inputs_and_rule.trim().split(" ");
-    if split_inputs_and_rule.clone().count() < 1 {
-        return error!(format!(
-            "Error while parsing inputs and rule: '{inputs_and_rule}'"
-        ));
-    }
-    let target_rule = String::from(split_inputs_and_rule.nth(0).unwrap());
-    let mut target_inputs: Vec<String> = Vec::new();
-    for input in split_inputs_and_rule {
-        target_inputs.push(String::from(input.trim()));
-    }
-    let mut target_implicit_dependencies: Vec<String> = Vec::new();
-    if let Some(implicit_dependencies) = split_pipe_input_and_dep_section.next() {
-        for implicit_dep in implicit_dependencies.trim().split(" ") {
-            target_implicit_dependencies.push(String::from(implicit_dep.trim()));
-        }
-    }
-    let mut target_order_only_dependencies: Vec<String> = Vec::new();
-    if let Some(order_only_dependencies) = split_pipe_input_section.next() {
-        for dep in order_only_dependencies.trim().split(" ") {
-            target_order_only_dependencies.push(String::from(dep.trim()));
-        }
-    }
     let mut target_variables: HashMap<String, String> = HashMap::new();
     while let Some(next_line) = lines.next() {
         if !next_line.starts_with("  ") {
