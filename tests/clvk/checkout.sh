ebd70350ebb28f0ff357a500539b032018230456
Romaric Jodin <rjodin@google.com>
2025-05-22 11:43:40 +0200

    clvk: add support for simple_test and api_tests

--- tests/clvk/checkout.sh
+++ tests/clvk/checkout.sh
@@ -6,4 +6,4 @@ set -xe
 DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-bash "${SCRIPT_DIR}/../../utils/checkout.sh" https://github.com/kpet/clvk a91d6a8092592f99971e6cd68fed8ecd57f19700 "${DEST}/external/clvk"
+bash "${SCRIPT_DIR}/../../utils/checkout.sh" https://github.com/rjodinchr/clvk d5304bc4f3735cb5b3eb6a6a180faab4708b3150 "${DEST}/external/clvk"
