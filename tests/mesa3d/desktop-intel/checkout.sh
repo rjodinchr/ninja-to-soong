4d21e62fa7b28c642a78cd48b37f135a84aa3349
Romaric Jodin <rjodin@google.com>
2025-07-15 09:55:59 +0200

    fix meson version for mesa (#201)
    https://github.com/rjodinchr/ninja-to-soong/pull/199 tried to fix the
    mesa version by installing it in the github action, but it ends up
    being reverted by the install happening in checkout.sh in mesa
    projects.
    
    Revert the github action, and update the checkout.sh scripts in mesa.

--- tests/mesa3d/desktop-intel/checkout.sh
+++ tests/mesa3d/desktop-intel/checkout.sh
@@ -10,7 +10,7 @@ bash "${SCRIPT_DIR}/../../../utils/checkout.sh" https://gitlab.freedesktop.org/m
 
 MAJOR_VERSION=17
 sudo apt install \
-     meson \
+     meson-1.5 \
      libclang-${MAJOR_VERSION}-dev \
      libclang-cpp${MAJOR_VERSION}{,-dev} \
      libclc-${MAJOR_VERSION}{,-dev} \
