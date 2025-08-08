5d0fb8b1de6b8e9145816c2850a86d548e21547a
Romaric Jodin <rjodin@google.com>
2025-08-08 08:07:28 +0200

    mesa: switch to libperfetto_client_experimental (#203)

--- tests/mesa3d/desktop-intel/gen-ninja.sh
+++ tests/mesa3d/desktop-intel/gen-ninja.sh
@@ -17,6 +17,7 @@ mkdir -p "${MESON_LOCAL_PATH}"
 ANDROID_PLATFORM="${ANDROID_PLATFORM}" NDK_PATH="${NDK_PATH}" \
 envsubst < "${SCRIPT_DIR}/${AOSP_X86_64}.template" > "${MESON_LOCAL_PATH}/${AOSP_X86_64}"
 
+PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${SCRIPT_DIR}" \
 PATH="${MESA_CLC_PATH}:${PATH}" \
 meson setup \
     --cross-file "${AOSP_X86_64}" \
@@ -29,6 +30,7 @@ meson setup \
     -Degl=enabled \
     -Dplatform-sdk-version=${ANDROID_PLATFORM} \
     -Dandroid-stub=true \
+    -Dandroid-libperfetto=enabled \
     -Dplatforms=android \
     -Dperfetto=true \
     -Degl-lib-suffix=_mesa \
