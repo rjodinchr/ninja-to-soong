829046b12780f629e516f2d52f4970579f4bf51d
Romaric Jodin <rjodin@google.com>
2025-08-20 13:51:25 +0200

    OpenCL-CTS: update to 909095f60a45d2ea131586a8a06411b3072a1bdd (#238)
    * OpenCL-CTS: update to 909095f60a45d2ea131586a8a06411b3072a1bdd
    
    * external-project: update to a simpler test example
    
    * allow opencl-cts to be cached
    
    * Update README

--- src/project/opencl_cts.rs
+++ src/project/opencl_cts.rs
@@ -4,7 +4,10 @@
 use super::*;
 
 #[derive(Default)]
-pub struct OpenclCts();
+pub struct OpenclCts {
+    src_path: PathBuf,
+    spirv_headers_path: PathBuf,
+}
 
 const DEFAULTS: &str = "OpenCL-CTS-defaults";
 const DEFAULTS_MANUAL: &str = "OpenCL-CTS-manual-defaults";
@@ -24,9 +27,9 @@ fn parse_test(line: &str) -> Option<String> {
     Some(String::from("test_conformance/") + binary)
 }
 
-fn parse_tests(src_path: &Path) -> Result<Vec<String>, String> {
+fn parse_tests(file_path: &Path) -> Result<Vec<String>, String> {
     let mut tests = Vec::new();
-    let content = read_file(&src_path.join("test_conformance/opencl_conformance_tests_full.csv"))?;
+    let content = read_file(file_path)?;
     let mut lines = content.lines();
     while let Some(line) = lines.next() {
         if line.is_empty() || line.starts_with("#") || line.starts_with("OpenCL-GL") {
@@ -58,31 +61,32 @@ impl Project for OpenclCts {
     fn generate_package(
         &mut self,
         ctx: &Context,
-        _projects_map: &ProjectsMap,
+        projects_map: &ProjectsMap,
     ) -> Result<String, String> {
-        let src_path = self.get_android_path(ctx)?;
+        self.src_path = self.get_android_path(ctx)?;
         let build_path = ctx.temp_path.join(self.get_name());
         let ndk_path = get_ndk_path(&ctx.temp_path, ctx)?;
+        self.spirv_headers_path = ProjectId::SpirvHeaders.get_android_path(projects_map, ctx)?;
 
-        let spirv_tools_path = if !ctx.skip_build {
-            ctx.temp_path.clone()
-        } else {
-            self.get_test_path(ctx)?
-        };
-
+        const CSV_FILENAME: &str = "opencl_conformance_tests_full.csv";
+        let csv_file_path = build_path.join(CSV_FILENAME);
         if !ctx.skip_gen_ninja {
             execute_cmd!(
                 "bash",
                 [
                     &path_to_string(self.get_test_path(ctx)?.join("gen-ninja.sh")),
-                    &path_to_string(&src_path),
+                    &path_to_string(&self.src_path),
                     &path_to_string(&build_path),
                     &path_to_string(&ndk_path),
-                    &path_to_string(&spirv_tools_path),
+                    &path_to_string(&self.spirv_headers_path),
                 ]
             )?;
+            write_file(
+                &csv_file_path,
+                &read_file(&self.src_path.join("test_conformance").join(CSV_FILENAME))?,
+            )?;
         }
-        let tests = parse_tests(&src_path)?
+        let tests = parse_tests(&csv_file_path)?
             .into_iter()
             .map(|test| {
                 (
@@ -109,7 +113,7 @@ impl Project for OpenclCts {
         .generate(
             NinjaTargetsToGenMap::from(&targets),
             parse_build_ninja::<CmakeNinjaTarget>(&build_path)?,
-            &src_path,
+            &self.src_path,
             &ndk_path,
             &build_path,
             None,
@@ -120,6 +124,9 @@ impl Project for OpenclCts {
         let gen_deps = package.get_gen_deps();
         let mut spirv_new_data = Vec::new();
         for dep in gen_deps {
+            if dep.ends_with("spirv.core.grammar.json") {
+                continue;
+            }
             let folder = file_name(dep.clone().parent().unwrap());
             let basename = PathBuf::from("test_conformance/spirv_new/spirv_asm");
             let (target_env, dirname) = if folder.starts_with("spv") {
@@ -188,7 +195,10 @@ impl Project for OpenclCts {
                 r#"
 cc_defaults {{
     name: "{DEFAULTS_MANUAL}",
-    header_libs: ["OpenCL-Headers"],
+    header_libs: [
+        "OpenCL-Headers",
+        "{2}",
+    ],
     cflags: [
         "-Wno-error",
         "-Wno-c++11-narrowing",
@@ -221,9 +231,14 @@ cc_test {{
                     .iter()
                     .map(|(_, test)| String::from("        \":") + test + "\",")
                     .collect::<Vec<_>>()
-                    .join("\n")
+                    .join("\n"),
+                CcLibraryHeaders::SpirvHeaders.str()
             ))
             .print(ctx)
+    }
+
+    fn get_deps_prefix(&self) -> Vec<(PathBuf, Dep)> {
+        vec![(self.spirv_headers_path.clone(), Dep::SpirvHeaders)]
     }
 
     fn extend_module(&self, target: &Path, mut module: SoongModule) -> Result<SoongModule, String> {
@@ -267,6 +282,9 @@ cc_test {{
             .add_prop("host_supported", SoongProp::Bool(true)))
     }
 
+    fn map_cmd_output(&self, output: &Path) -> PathBuf {
+        PathBuf::from(file_name(output))
+    }
     fn map_lib(&self, lib: &Path) -> Option<PathBuf> {
         if lib.ends_with("libOpenCL") {
             return Some(PathBuf::from("//external/OpenCL-ICD-Loader:libOpenCL"));
@@ -277,11 +295,12 @@ cc_test {{
     fn filter_cflag(&self, _cflag: &str) -> bool {
         false
     }
-    fn filter_gen_header(&self, _header: &Path) -> bool {
-        false
+    fn filter_gen_header(&self, header: &Path) -> bool {
+        header.ends_with("spirv_capability_deps.def")
     }
     fn filter_include(&self, include: &Path) -> bool {
         !include.ends_with("OpenCL-Headers")
+            && path_to_string(include).starts_with(&path_to_string(&self.src_path))
     }
     fn filter_lib(&self, lib: &str) -> bool {
         !lib.contains("atomic")
--- tests/OpenCL-CTS/gen-ninja.sh
+++ tests/OpenCL-CTS/gen-ninja.sh
@@ -7,15 +7,16 @@ set -xe
 SRC_PATH="$1"
 BUILD_PATH="$2"
 NDK_PATH="$3"
-SPIRV_TOOLS_PATH="$4"
+SPIRV_HEADERS_PATH="$4"
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-PATH="${SPIRV_TOOLS_PATH}:${PATH}" \
+PATH="${SCRIPT_DIR}:${PATH}" \
 bash "${SCRIPT_DIR}/../../utils/cmake_configure.sh" \
     "${SRC_PATH}" \
     "${BUILD_PATH}" \
     "${NDK_PATH}" \
+    -DSPIRV_INCLUDE_DIR="${SPIRV_HEADERS_PATH}" \
     -DCL_INCLUDE_DIR="${SRC_PATH}/../OpenCL-Headers" \
     -DCL_LIB_DIR="${SRC_PATH}" \
     -DOPENCL_LIBRARIES="-lOpenCL"
