73afd0e20c6b59100c74656b85346d1d56fec3df
Romaric Jodin <rjodin@google.com>
2025-08-20 13:18:51 +0200

    external-project: update to a simpler test example
    Change-Id: I5901ba53a4e25f108d5361ab440860dc55d59ab4

---
+++ tests/external-project/project.rs
@@ -0,0 +1,56 @@
+// Copyright 2025 ninja-to-soong authors
+// SPDX-License-Identifier: Apache-2.0
+
+extern crate ninja_to_soong;
+use ninja_to_soong::context::*;
+use ninja_to_soong::ninja_parser::*;
+use ninja_to_soong::ninja_target::*;
+use ninja_to_soong::project::*;
+use ninja_to_soong::soong_package::*;
+use ninja_to_soong::utils::*;
+
+#[no_mangle]
+pub fn get_project() -> Box<dyn Project> {
+    Box::new(ExternalProject::default())
+}
+
+#[derive(Default)]
+pub struct ExternalProject();
+
+impl Project for ExternalProject {
+    fn get_name(&self) -> &'static str {
+        "external-project"
+    }
+    fn get_android_path(&self, _ctx: &Context) -> Result<PathBuf, String> {
+        error!("Not implemented")
+    }
+    fn get_test_path(&self, ctx: &Context) -> Result<PathBuf, String> {
+        Ok(PathBuf::from(
+            ctx.get_external_project_path()?.parent().unwrap(),
+        ))
+    }
+    fn generate_package(
+        &mut self,
+        ctx: &Context,
+        _projects_map: &ProjectsMap,
+    ) -> Result<String, String> {
+        let test_path = self.get_test_path(ctx)?;
+        SoongPackage::new(
+            &["//visibility:public"],
+            "external-project_license",
+            &["SPDX-license-identifier-Apache-2.0"],
+            &["LICENSE"],
+        )
+        .generate(
+            NinjaTargetsToGenMap::from(&[target!("external_project_library")]),
+            parse_build_ninja::<CmakeNinjaTarget>(&test_path)?,
+            &test_path,
+            &PathBuf::from("ndk"),
+            &test_path,
+            None,
+            self,
+            ctx,
+        )?
+        .print(ctx)
+    }
+}
