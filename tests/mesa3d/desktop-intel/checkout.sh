12c6fd54bba0075cc38e832fc608f2ab7bd679a0
Romaric Jodin <rjodin@google.com>
2025-10-16 09:06:18 +0200

    mesa3d: desktop-intel: apply patches merged upstream
    Change-Id: I050b6fd3c9f718f1eabf92c7d481cbc233479c10

--- tests/mesa3d/desktop-intel/checkout.sh
+++ tests/mesa3d/desktop-intel/checkout.sh
@@ -9,6 +9,9 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 bash "${SCRIPT_DIR}/../../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa 7fc153c93fd5e44643dc53a7b5189896b67aed2b "${DEST}/vendor/google/graphics/mesa3d/desktop-intel"
 
 for patch in \
+    "mesa-0719638dfdc.patch" \
+    "mesa-d0de915c0c7.patch" \
+    "mesa-ed0c18ae4aa.patch" \
     "mesa-a8ab696033e.patch" \
     "mesa-2f9fd1768ae.patch" \
     "mesa-c8b10b4512c.patch" 
