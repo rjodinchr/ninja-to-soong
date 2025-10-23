3a4fd556fb12c8f4b85fd7c3045dc8a746abfb0a
Romaric Jodin <rjodin@google.com>
2025-10-23 10:10:01 +0200

    mesa3d: desktop-intel: update CI to 25.2.4 (#281)

--- tests/mesa3d/desktop-intel/checkout.sh
+++ tests/mesa3d/desktop-intel/checkout.sh
@@ -7,7 +7,7 @@ DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 DEST_DIR="${DEST}/vendor/google/graphics/mesa3d/desktop-intel"
 
-bash "${SCRIPT_DIR}/../../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa 62f9be9a657a493b8416534feea20294d0e98539 "${DEST_DIR}"
+bash "${SCRIPT_DIR}/../../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa ab462ae6b7064ed63b8c94f31d5e3be60dcfede6 "${DEST_DIR}"
 
 for patch in \
     "mesa-0719638dfdc.patch" \
