689f214fd39b3924738f6e125c1ecfcab64d5fa0
Andy Hsu <hwandy@google.com>
2025-11-24 18:25:16 +0800

    mesa3d: desktop-intel: Update test.

--- tests/mesa3d/desktop-intel/checkout.sh
+++ tests/mesa3d/desktop-intel/checkout.sh
@@ -25,7 +25,7 @@ done
 
 MAJOR_VERSION=19
 sudo apt install \
-     meson-1.5 \
+     meson \
      libclang-${MAJOR_VERSION}-dev \
      libclang-cpp${MAJOR_VERSION}{,-dev} \
      libclc-${MAJOR_VERSION}{,-dev} \
