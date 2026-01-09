2ee9695bdb5004d3f7f6702671aa7e918f4d8d7b
Yiwei Zhang <zzyiwei@gmail.com>
2026-01-09 15:28:00 -0800

    desktop-panvk: update to 25.3.3 (#315)
    Co-authored-by: Yiwei Zhang <zzyiwei@google.com>

--- tests/mesa3d/desktop-panvk/checkout.sh
+++ tests/mesa3d/desktop-panvk/checkout.sh
@@ -7,10 +7,9 @@ DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 DEST_DIR="${DEST}/vendor/google/graphics/mesa3d/desktop-panvk"
 
-bash "${SCRIPT_DIR}/../../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa 29ccbb21f30553d6a39967ae013748063b73005a "${DEST_DIR}"
+bash "${SCRIPT_DIR}/../../checkout.sh" https://gitlab.freedesktop.org/mesa/mesa ad3759f342ee988929ef32022d5486f99484221f "${DEST_DIR}"
 
 for patch in \
-    "mesa-c8b10b4512c.patch" \
     "mesa-762be5eae1e.patch"
 do
     git -C "${DEST_DIR}" apply "${SCRIPT_DIR}/../patches/${patch}"
