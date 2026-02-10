8c33144f8f9ae76878641f187dbaaadff9295099
Romaric Jodin <rjodin@google.com>
2026-02-10 11:18:45 +0100

    Update clspv & dependencies
    Change-Id: Ie7680dedce081582963b434426b48f32351b68fc

--- tests/llvm-project/checkout.sh
+++ tests/llvm-project/checkout.sh
@@ -6,4 +6,4 @@ set -xe
 DEST="$1"
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-bash "${SCRIPT_DIR}/../checkout.sh" https://github.com/llvm/llvm-project 42a8ff877d47131ecb1280a1cc7e5e3c3bca6952 "${DEST}/external/opencl/llvm-project"
+bash "${SCRIPT_DIR}/../checkout.sh" https://github.com/llvm/llvm-project d43b29fc545d702b35b20802f92357bc4c4177fe "${DEST}/external/opencl/llvm-project"
